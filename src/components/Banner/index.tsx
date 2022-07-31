3c0810b25514cc50120efd61b904dde97ea2cb53
JCRUZ-Dev <juancarloscruz278@gmail.com>
2022-07-31 03:38:39 -0400

    ✨  refactor code

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,22 +1,27 @@
-import { AtomIcon, AtomText, AtomWrapper } from '@stacklycore/ui';
 import { BannerStyled, BannerVideo } from '@Src/components/Banner/styled';
+import { AtomIcon, AtomText, AtomWrapper } from '@stacklycore/ui';
 
 const Banner = () => {
   return (
-    <AtomWrapper css={BannerStyled}>
-      <AtomWrapper className={"banner-content"}>
-        <AtomText as={"h1"} className={"banner-title"}>
+    <AtomWrapper as="section" css={BannerStyled}>
+      <AtomWrapper className={'banner-content'}>
+        <AtomText as={'h1'} className={'banner-title'}>
           Recordar tu infancia, es volver a vivir.
         </AtomText>
-        <AtomText as={"p"} className={"banner-description"}>
-          Aquí podras ver las series y caricaturas que cultivaron tu niñez frente al televisor.
+        <AtomText as={'p'} className={'banner-description'}>
+          Aquí podras ver las series y caricaturas que cultivaron tu niñez
+          frente al televisor.
         </AtomText>
       </AtomWrapper>
       <AtomWrapper css={BannerVideo}>
-        <AtomIcon icon={"https://storage.googleapis.com/cdn-bucket-ixulabs-platform/STCO-0001/playnew.svg"} className={"banner-video-icon"}/>
+        <AtomIcon
+          icon={
+            'https://storage.googleapis.com/cdn-bucket-ixulabs-platform/STCO-0001/playnew.svg'
+          }
+          className={'banner-video-icon'}
+        />
       </AtomWrapper>
-
     </AtomWrapper>
-  )
-}
-export default Banner
+  );
+};
+export default Banner;
