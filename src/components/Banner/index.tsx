a4778f0eb1320450f54ef4ddcdc19d0421c9ce60
JCRUZ-Dev <juancarloscruz278@gmail.com>
2022-08-24 21:30:59 -0400

    refactor: changes Banner structure

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,26 +1,11 @@
-import { BannerStyled, BannerVideo } from '@Src/components/Banner/styled';
-import { AtomIcon, AtomText, AtomWrapper } from '@stacklycore/ui';
+import { BannerStyled } from '@Src/components/Banner/styled';
+import { AtomWrapper } from '@stacklycore/ui';
+import { FC, ReactNode } from 'react';
 
-const Banner = () => {
+const Banner: FC<{ children: ReactNode }> = ({ children }) => {
   return (
     <AtomWrapper as="section" css={BannerStyled}>
-      <AtomWrapper className={'banner-content'}>
-        <AtomText as={'h1'} className={'banner-title'}>
-          Recordar tu infancia, es volver a vivir.
-        </AtomText>
-        <AtomText as={'p'} className={'banner-description'}>
-          Aquí podras ver las series y caricaturas que cultivaron tu niñez
-          frente al televisor.
-        </AtomText>
-      </AtomWrapper>
-      <AtomWrapper css={BannerVideo}>
-        <AtomIcon
-          icon={
-            'https://storage.googleapis.com/cdn-bucket-ixulabs-platform/STCO-0001/playnew.svg'
-          }
-          className={'banner-video-icon'}
-        />
-      </AtomWrapper>
+      {children}
     </AtomWrapper>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,32 @@
 import Banner from '@Src/components/Banner';
+import { BannerVideo } from '@Src/components/Banner/styled';
 import Cartoons from '@Src/components/Cartoons';
 import Channels from '@Src/components/Channels';
-import Header from '@Src/components/Header';
+import { AtomIcon, AtomText, AtomWrapper } from '@stacklycore/ui';
 import { NextPageFC } from 'next';
 
 const PageIndex: NextPageFC = () => {
   return (
     <>
-      <Header />
-      <Banner />
+      <Banner>
+        <AtomWrapper className={'banner-content'}>
+          <AtomText as={'h1'} className={'banner-title'}>
+            Recordar tu infancia, es volver a vivir.
+          </AtomText>
+          <AtomText as={'p'} className={'banner-description'}>
+            Aquí podras ver las series y caricaturas que cultivaron tu niñez
+            frente al televisor.
+          </AtomText>
+        </AtomWrapper>
+        <AtomWrapper css={BannerVideo}>
+          <AtomIcon
+            icon={
+              'https://storage.googleapis.com/cdn-bucket-ixulabs-platform/STCO-0001/playnew.svg'
+            }
+            className={'banner-video-icon'}
+          />
+        </AtomWrapper>
+      </Banner>
       <Channels />
       <Cartoons />
     </>
