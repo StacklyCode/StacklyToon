cfdab62e6fd28c22f08258d21ec95caffe1868e0
JCRUZ-Dev <juancarloscruz278@gmail.com>
2022-08-28 00:10:18 -0400

    fixes series by ID query

--- src/apollo/query/serieById.ts
+++ src/apollo/query/serieById.ts
@@ -1,27 +1,28 @@
 import { gql } from '@apollo/client';
 
 export const SERIESBYID = gql`
- query($id: UUID!){
- serieById(serieId: $id) {
-   title
-   image
-   language {
-     name
-   }
-   studio{
-     name
-   }
-   productionYear
-   seasons{
-     episodes{
-       video
-       image
-       title
-       id
-       season {
-         title
-       }
-     }
-   }
- }
+  query ($id: UUID!) {
+    serieById(serieId: $id) {
+      title
+      image
+      language {
+        name
+      }
+      studio {
+        name
+      }
+      productionYear
+      seasons {
+        episodes {
+          video
+          image
+          title
+          id
+          season {
+            title
+          }
+        }
+      }
+    }
+  }
 `;
